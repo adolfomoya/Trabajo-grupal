66435ce38a23113337b32cdb5227a06504da4da0
ridolf-desing <74732924+ridolf-desing@users.noreply.github.com>
2020-12-11 08:21:48 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+	<style type="text/css">
+		body{
+			background-color: #D9F44F;
+		}
+	</style>
+	<ul class="nav">
+  		<li class="nav-item">
+   		 	<a class="nav-link active" href="Principal.php">Principal</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link" href="Productos.php">Productos</a>
+  		</li>
+  		<li class="nav-item">
+   			<a class="nav-link" href="Nosotros.php">Nosotros</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link disabled" href="Ofertas.php" tabindex="-1" aria-disabled="true">Ofertas</a>
+  		</li>
+	</ul>
+	<br>
+	<ul class="nav justify-content-center">
+  		<li class="nav-item">
+   		 	<a class="nav-link active" href="Principal.php">Principal</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link" href="Productos.php">Productos</a>
+  		</li>
+  		<li class="nav-item">
+   			<a class="nav-link" href="Nosotros.php">Nosotros</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link disabled" href="Ofertas.php" tabindex="-1" aria-disabled="true">Ofertas</a>
+  		</li>
+	</ul>
+	<br>
+	<ul class="nav justify-content-end">
+  		<li class="nav-item">
+   		 	<a class="nav-link active" href="Principal.php">Principal</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link" href="Productos.php">Productos</a>
+  		</li>
+  		<li class="nav-item">
+   			<a class="nav-link" href="Nosotros.php">Nosotros</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link disabled" href="Ofertas.php" tabindex="-1" aria-disabled="true">Ofertas</a>
+  		</li>
+	</ul>
+	<br>
+	<ul class="nav flex-column">
+  		<li class="nav-item">
+   		 	<a class="nav-link active" href="Principal.php">Principal</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link" href="Productos.php">Productos</a>
+  		</li>
+  		<li class="nav-item">
+   			<a class="nav-link" href="Nosotros.php">Nosotros</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link disabled" href="Ofertas.php" tabindex="-1" aria-disabled="true">Ofertas</a>
+  		</li>
+	</ul>
+	<br>
+	<ul class="nav nav-tabs">
+  		<li class="nav-item">
+   		 	<a class="nav-link active" href="Principal.php">Principal</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link" href="Productos.php">Productos</a>
+  		</li>
+  		<li class="nav-item">
+   			<a class="nav-link" href="Nosotros.php">Nosotros</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link disabled" href="Ofertas.php" tabindex="-1" aria-disabled="true">Ofertas</a>
+  		</li>
+	</ul>
+	<br>
+	<ul class="nav nav-pills">
+  		<li class="nav-item">
+   		 	<a class="nav-link active" href="Principal.php">Principal</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link" href="Productos.php">Productos</a>
+  		</li>
+  		<li class="nav-item">
+   			<a class="nav-link" href="Nosotros.php">Nosotros</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link disabled" href="Ofertas.php" tabindex="-1" aria-disabled="true">Ofertas</a>
+  		</li>
+	</ul>
+	<br>
+	<ul class="nav nav-pills nav-fill">
+  		<li class="nav-item">
+   		 	<a class="nav-link active" href="Principal.php">Principal</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link" href="Productos.php">Productos</a>
+  		</li>
+  		<li class="nav-item">
+   			<a class="nav-link" href="Nosotros.php">Nosotros</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link disabled" href="Ofertas.php" tabindex="-1" aria-disabled="true">Ofertas</a>
+  		</li>
+	</ul>
+	<br>
+	<ul class="nav nav-pills nav-justified">
+  		<li class="nav-item">
+   		 	<a class="nav-link active" href="Principal.php">Principal</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link" href="Productos.php">Productos</a>
+  		</li>
+  		<li class="nav-item">
+   			<a class="nav-link" href="Nosotros.php">Nosotros</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link disabled" href="Ofertas.php" tabindex="-1" aria-disabled="true">Ofertas</a>
+  		</li>
+	</ul>
+	<br>
+	<ul class="nav nav-tabs">
+  		<li class="nav-item">
+    		<a class="nav-link active" href="Principal.php">Principal</a>
+  		</li>
+  		<li class="nav-item dropdown">
+    		<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Productos</a>
+    	<div class="dropdown-menu">
+      		<a class="dropdown-item" href="#">ropas</a>
+      		<a class="dropdown-item" href="#">Telefonos</a>
+     		<a class="dropdown-item" href="#">otros</a>
+        <div class="dropdown-divider"></div>
+      		<a class="dropdown-item" href="#">Nuestras redes</a>
+    	</div>
+  		</li>
+  		<li class="nav-item">
+   			<a class="nav-link" href="Nosotros.php">Nosotros</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link disabled" href="Ofertas.php" tabindex="-1" aria-disabled="true">Ofertas</a>
+  		</li>
+	</ul>
+	<br>
+	<ul class="nav nav-tabs" id="myTab" role="tablist">
+  		<li class="nav-item" role="presentation">
+    		<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Principal</a>
+  		</li>
+  		<li class="nav-item" role="presentation">
+    		<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Productos</a>
+  		</li>
+  		<li class="nav-item" role="presentation">
+    		<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Nosotros</a>
+  		</li>
+	</ul>
+	<div class="tab-content" id="myTabContent">
+  		<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">Esta es la pagina principal</div>
+  		<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Estos son nuestros productos</div>
+  		<div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">Mas sobre Nosotros</div>
+	</div>
+	
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
